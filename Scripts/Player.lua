893f7df0c2faaa196c3394234f4d408ba1b33343
MasterLaplace <guillaume.papineau@epitech.eu>
2024-01-16 23:30:31 +0100

    fix(Scripts): Refactor player movement logic and add network IP check

--- Scripts/Player.lua
+++ Scripts/Player.lua
@@ -1,7 +1,6 @@
 
 function scriptLua(entity)
 
-    --print("scriptLua called")
     movementManager(entity)
     boss(entity)
 end
@@ -22,6 +21,7 @@ function movementManager(entity)
     local fireRate = getFireRate(entity)
     local controlComponent = getComponentControl(entity)
     local clock = getElapsedTime(entity)
+    local networkIp = getIpByComponentNetworkIp(entity)
 
     local MOVE_LEFT = 10
     local MOVE_RIGHT = 11
@@ -41,41 +41,43 @@ function movementManager(entity)
             playSound()
         end
 
-        if getInput(entity, 71) and controlComponent.left then
-            movableComponent.velocity.x = -1.0
-            addEvent(MOVE_LEFT, PRESSED);
-        elseif getInput(entity, 72) and controlComponent.right then
-            movableComponent.velocity.x = 1.0
-            addEvent(MOVE_RIGHT, PRESSED);
-        else
-            movableComponent.velocity.x = 0.0
-            addEvent(MOVE_LEFT, RELEASED);
-            addEvent(MOVE_RIGHT, RELEASED);
-        end
-
-        if getInput(entity, 73) and controlComponent.up then
-            movableComponent.velocity.y = -1.0
-            addEvent(MOVE_UP, PRESSED);
-        elseif getInput(entity, 74) and controlComponent.down then
-            movableComponent.velocity.y = 1.0
-            addEvent(MOVE_DOWN, PRESSED);
-        else
-            movableComponent.velocity.y = 0.0
-            addEvent(MOVE_UP, RELEASED);
-            addEvent(MOVE_DOWN, RELEASED);
-        end
-
-        if getInput(entity, 57) and controlComponent.shoot then
-            if clock > fireRate then
-                restartClock(entity)
-                createBullet(entity, transformComponent)
-                addEvent(SHOOT, PRESSED);
-            end
-        else
-            if getEventState(SHOOT, PRESSED) then
-                addEvent(SHOOT, RELEASED);
+        if networkIp == "undefined" or networkIp ~= "" then
+            if getInput(entity, 71) and controlComponent.left then
+                movableComponent.velocity.x = -1.0
+                addEvent(MOVE_LEFT, PRESSED);
+            elseif getInput(entity, 72) and controlComponent.right then
+                movableComponent.velocity.x = 1.0
+                addEvent(MOVE_RIGHT, PRESSED);
             else
-                addEvent(SHOOT, NONE);
+                movableComponent.velocity.x = 0.0
+                addEvent(MOVE_LEFT, RELEASED);
+                addEvent(MOVE_RIGHT, RELEASED);
+            end
+
+            if getInput(entity, 73) and controlComponent.up then
+                movableComponent.velocity.y = -1.0
+                addEvent(MOVE_UP, PRESSED);
+            elseif getInput(entity, 74) and controlComponent.down then
+                movableComponent.velocity.y = 1.0
+                addEvent(MOVE_DOWN, PRESSED);
+            else
+                movableComponent.velocity.y = 0.0
+                addEvent(MOVE_UP, RELEASED);
+                addEvent(MOVE_DOWN, RELEASED);
+            end
+
+            if getInput(entity, 57) and controlComponent.shoot then
+                if clock > fireRate then
+                    restartClock(entity)
+                    createBullet(entity, transformComponent)
+                    addEvent(SHOOT, PRESSED);
+                end
+            else
+                if getEventState(SHOOT, PRESSED) then
+                    addEvent(SHOOT, RELEASED);
+                else
+                    addEvent(SHOOT, NONE);
+                end
             end
         end
     end
